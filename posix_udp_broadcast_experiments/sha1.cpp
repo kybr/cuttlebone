93ed6d3156234e053668673a4419db8af0320d34
Karl Yerkes <karl.yerkes@gmail.com>
2013-10-29 13:45:49 -0700

    deleted example of sha1

--- posix_udp_broadcast_experiments/sha1.cpp
+++
@@ -1,8 +0,0 @@
-// c++ sha1.cpp -lcrypto
-//
-#include <openssl/sha.h>
-int main() {
-  const unsigned char d[] = "Original String";
-  unsigned char md[SHA_DIGEST_LENGTH];
-  SHA1(d, sizeof(d) - 1, md);
-}
