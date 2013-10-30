08705100839d4be4f198687ff272a78e724df6f9
Karl Yerkes <karl.yerkes@gmail.com>
2013-10-30 09:36:04 -0700

    added c++11 threads

--- posix_udp_broadcast_experiments/app.cpp
+++ posix_udp_broadcast_experiments/app.cpp
@@ -1,31 +1,35 @@
 #include "Stuff.hpp"
 #include <cassert>
 #include <thread>
+using namespace std;
 
 #define N (1024)
 #define WAIT (100000)
 
 int main() {
   unsigned char sent[N];
-  unsigned char received[N];
   Writer<N> writer;
-  Reader<N> reader;
-
   writer.init();
-  reader.init();
-
-  for (int i = 0; i < N; i++)
-    sent[i] = i;
-
+  for (int i = 0; i < N; i++) sent[i] = i;
   unsigned n = 0;
-  while (true) {
+
+  thread foo([]() {
+    unsigned char received[N];
+    Reader<N> reader;
+    reader.init();
+    for (int k = 0; k < 10; k++) {
+      reader.poll(received);
+      printf(" %03u\n", received[0]);
+      usleep(WAIT);
+    }
+  });
+
+  for (int k = 0; k < 10; k++) {
     writer.send(sent);
     printf("%03u ->", sent[0]);
-    for (int i = 0; i < N; i++)
-      sent[i]++;
-    usleep(WAIT);
-    reader.poll(received);
-    printf(" %03u\n", received[0]);
+    for (int i = 0; i < N; i++) sent[i]++;
     usleep(WAIT);
   }
+
+  foo.join();
 }
