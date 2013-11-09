61a108527dc72113ba61f335aaaf2b996234c5a6
Karl Yerkes <karl.yerkes@gmail.com>
2013-11-09 14:12:26 -0800

    added queue class

---
+++ Framework/Queue.hpp
@@ -0,0 +1,47 @@
+#ifndef __QUEUE__
+#define __QUEUE__
+
+#include <atomic>
+
+template <typename DATA, size_t SIZE = 5>
+class Queue {
+  static_assert(std::is_pod<DATA>::value, "DATA must be a POD type.");
+
+  enum {
+    CAPACITY = SIZE + 1
+  };
+
+ private:
+  inline size_t increment(size_t i) const { return (i + 1) % CAPACITY; }
+  std::atomic<size_t> _tail;  // tail(input) index
+  std::atomic<size_t> _head;  // head(output) index
+  DATA _array[CAPACITY];
+
+ public:
+  Queue() : _tail(0), _head(0) {}
+  virtual ~Queue() {}
+
+  bool push(const DATA& item) {  // pushByMOve?
+    const auto current_tail = _tail.load(std::memory_order_relaxed);
+    const auto next_tail = increment(current_tail);
+    if (next_tail != _head.load(std::memory_order_acquire)) {
+      _array[current_tail] = item;
+      _tail.store(next_tail, std::memory_order_release);
+      return true;
+    }
+    return false;  // full queue
+  }
+
+  // Pop by Consumer can only update the head (load with relaxed, store with
+  // release) the tail must be accessed with at least aquire
+  bool pop(DATA& item) {
+    const auto current_head = _head.load(std::memory_order_relaxed);
+    if (current_head == _tail.load(std::memory_order_acquire))
+      return false;  // empty queue
+    item = _array[current_head];
+    _head.store(increment(current_head), std::memory_order_release);
+    return true;
+  }
+};
+
+#endif
---
+++ test/queue.cpp
@@ -0,0 +1,99 @@
+#include "Framework/Queue.hpp"
+
+#include <unistd.h>  // usleep
+#include <iostream>  // cout
+#include <thread>
+using namespace std;
+
+//#include <cstddef>
+//#include <string>
+//#include <stdlib.h>
+
+// must be a POD type
+//
+struct State {
+  unsigned frame;
+  char data[256];
+  void zero() { memset(this, 0, sizeof(State)); }
+};
+
+int main(int argc, char* argv[]) {
+  Queue<State> simulateBroadcast, receiveRender;
+  thread simulate, broadcast, receive, render;
+
+  unsigned simulateBroadcastWasFull = 0;
+  unsigned simulateBroadcastWasEmpty = 0;
+  unsigned receiveRenderWasFull = 0;
+  unsigned receiveRenderWasEmpty = 0;
+
+  bool waitingToStart = true, done = false;
+
+  simulate = thread([&]() {
+    State s;
+    s.zero();
+    while (waitingToStart)
+      usleep(1000);
+    while (!done) {
+      sprintf(s.data, "%u", s.frame);
+      if (simulateBroadcast.push(s))
+        s.frame++;
+      else
+        simulateBroadcastWasFull++;
+      usleep(16666);
+    }
+  });
+
+  broadcast = thread([&]() {
+    State s;
+    while (waitingToStart)
+      usleep(1000);
+    while (!done) {
+      if (simulateBroadcast.pop(s)) {
+        // cut s into pieces and broadcast
+      } else
+        simulateBroadcastWasEmpty++;
+      usleep(16666);
+    }
+  });
+
+  receive = thread([&]() {
+    State s;
+    while (waitingToStart)
+      usleep(1000);
+    while (!done) {
+      // select with a small timeout
+      // receive lots of little pieces of s and assemble
+      // indicate when we lost a piece
+      // when s is ready, then push
+      if (!receiveRender.push(s))
+        receiveRenderWasFull++;
+      usleep(1000);
+    }
+  });
+
+  render = thread([&]() {
+    State s;
+    while (waitingToStart)
+      usleep(1000);
+    while (!done) {
+      if (receiveRender.pop(s)) {
+        // make a pretty picture with s
+      } else
+        receiveRenderWasEmpty++;
+      usleep(16666);
+    }
+  });
+
+  waitingToStart = false;
+  getchar();
+  done = true;
+  simulate.join();
+  broadcast.join();
+  receive.join();
+  render.join();
+
+  cout << "simulateBroadcastWasFull:" << simulateBroadcastWasFull << endl;
+  cout << "simulateBroadcastWasEmpty:" << simulateBroadcastWasEmpty << endl;
+  cout << "receiveRenderWasFull:" << receiveRenderWasFull << endl;
+  cout << "receiveRenderWasEmpty:" << receiveRenderWasEmpty << endl;
+}
